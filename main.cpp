ec1d3bdbd9b02ce24c2823a6806e34ca801d7d0a
Deception101 <kytenjohnson@sheridan.edu>
2026-09-18 05:12:13 +0000

    added main and some comments

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 
 using namespace std;
 
+//represents a music artist
 class Artist
 {
 private:
@@ -14,320 +15,166 @@ private:
     string country;
     int albums;
 
-    public: 
-        Artist(string n, string g, string c,int a)
-        {
-            name = n;
-            genre = g;
-            country = c;
-            albums = a;
-        }
+public:
+//constructor, gives values to Artist's objects
+    Artist(string n, string g, string c, int a)
+    {
+        name = n;
+        genre = g;
+        country = c;
+        albums = a;
+    }
+//Accessors, returns the artist's info
+    string getName() const
+    {
+        return name;
+    }
 
-        String getName() const
-        {
-            Return name;
-        }
+    string getGenre() const
+    {
+        return genre;
+    }
 
-        String getGenre() const
-        {
-            Return genre;
-        }
+    string getCountry() const
+    {
+        return country;
+    }
 
-        String getCountry() const
-        {
-            Return country;
-        }
+    int getAlbums() const
+    {
+        return albums;
+    }
 
-        Int getAlbums() const
-        {
-            Return albums;
-        }
+    //mutator, sets the artist's info
+    void setName(string n)
+    {
+        name = n;
+    }
 
-        Void setName(string n)
-        {
-            Name = n;
-        }
+    void setGenre(string g)
+    {
+        genre = g;
+    }
 
-        Void setGenre(string g)
-        {
-            Genre = g;
-        }
+    void setCountry(string c)
+    {
+        country = c;
+    }
 
-        Void setCountry(string c)
-        {
-            Country = c;
-        }
+    void setAlbums(int a)
+    {
+        albums = a;
+    }
 
-        Void setAlbums(int a)
-        {
-            Albums = a;
-        }
-
-        Void display() const
-        {
-            Cout << “Artist: “ << name << endl;
-            Cout << “Genre: “ << genre << endl;
-            Cout << “Country: “ << country << endl;
-            Cout << “Albums: “ << albums << endl;
-        }
+    //prints all the artist's info
+    void display() const
+    {
+        cout << "Artist: " << name << endl;
+        cout << "Genre: " << genre << endl;
+        cout << "Country: " << country << endl;
+        cout << "Albums: " << albums << endl;
+    }
 };
 
-Class Song
+class Song
 {
-Private:
-    String title;
+private:
+    string title;
     Artist artist;
-    String album;
-    Int duration;
-    String genre;
-    Int releaseYear;
+    string album;
+    int duration;
+    string genre;
+    int releaseYear;
 
-Public:
+public:
     Song(string t, Artist a, string al, int d, string g, int r)
     {
-        Title = t;
-        Artist = a;
-        Album = al;
-        Duration = d;
-        Genre = g;
+        title = t;
+        artist = a;
+        album = al;
+        duration = d;
+        genre = g;
         releaseYear = r;
     }
 
-    String getTitle() const
+    string getTitle() const
     {
-        Return title;
+        return title;
     }
 
     Artist getArtist() const
     {
-        Return artist;
+        return artist;
     }
 
-    String getAlbum() const
+    string getAlbum() const
     {
-        Return album;
+        return album;
     }
 
-    Int getDuration() const
+    int getDuration() const
     {
-        Return duration;
+        return duration;
     }
 
-    String getGenre() const
+    string getGenre() const
     {
-        Return genre;
+        return genre;
     }
 
-    Int getReleaseYear() const
+    int getReleaseYear() const
     {
-        Return releaseYear;
+        return releaseYear;
     }
 
-    Void setTitle(string t)
+    void setTitle(string t)
     {
-        Title = t;
+        title = t;
     }
 
-    Void setArtist(Artist a)
+    void setArtist(Artist a)
     {
-        Artist = a;
+        artist = a;
     }
 
-    Void setAlbum(string al)
+    void setAlbum(string al)
     {
-        Album = al;
+        album = al;
     }
 
-    Void setDuration(int d)
+    void setDuration(int d)
     {
-        Duration = d;
+        duration = d;
     }
 
-    Void setGenre(string g)
+    void setGenre(string g)
     {
-        Genre = g;
+        genre = g;
     }
 
-    Void setReleaseYear(int r)
+    void setReleaseYear(int r)
     {
         releaseYear = r;
     }
 
-    Void display() const
+    void display() const
     {
-        Cout << “Title: “ << title << endl;
+        cout << "Title: " << title << endl;
         artist.display();
-        Cout << “Album: “ << album << endl;
-        Cout << “Duration: “ << duration << “ seconds” << endl;
-        Cout << “Genre: “ << genre << endl;
-        Cout << “Release Year: “ << releaseYear << endl;
+        cout << "Album: " << album << endl;
+        cout << "Duration: " << duration << " seconds" << endl;
+        cout << "Genre: " << genre << endl;
+        cout << "Release Year: " << releaseYear << endl;
     }
 };
 
+int main()
+{
+    Artist artist("Colby Acuff", "Country", "USA", 5);
 
+    Song song("If I Were the Devil", artist, "Western White Pines", 220, "Country", 2022);
 
-void printMenu();
-void addTrack(vector< MusicTrack> & library);
-void displayLibrary(const vector<MusicTrack>&library);
-void saveLibraryToFile(const vector<MusicTrack>& library, const string& filename);
-void loadLibraryFromFile(vector<MusicTrack> & library, const string& filename);
-
-
-int main() {
-    vector<MusicTrack> library;
-    const string filename = "musiclibrary.dat";
-    int choice = -1;
-
-    do {
-        printMenu();
-        cin >> choice;
-
-        if (cin.fail()) {
-            cin.clear();
-            cin.ignore(numeric_limits<streamsize>::max(), '\n');
-            cout << "Invalid input. Please enter a number.\n\n";
-            continue;
-
-        }
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-
-// loops the options until the users exits
-        switch (choice) {
-            case 1: addTrack(library); break;
-            case 2: displayLibrary(library); break;
-            case 3: saveLibraryToFile(library,filename); break;
-            case 4: loadLibraryFromFile(library,filename); break;
-            case 5: cout << "Exiting Music Library. Goodbye\n"; break;
-            default: cout << "Invalid choice. Select a valid option\n\n";
-        }
-
-}
-    while (choice !=5);
+    song.display();
 
     return 0;
-}
-
-void printMenu() {
-    cout << "==== Music Library Menu ====\n";
-    cout << "1. Add Track\n";
-    cout << "2. Display Library\n";
-    cout << "3. Save Library to File\n";
-    cout << "4. Load Library from File\n";
-    cout << "5. Exit\n";
-    cout << "Enter your choice: " ;
-}
-
-
-void addTrack(vector<MusicTrack>& library) {
-    // Asks the user for each field and adds the new track to the library
-    MusicTrack track;
-
-    cout << "Enter track title: ";
-    getline(cin,track.title);
-
-    cout << "Enter artist: ";
-    getline(cin,track.artist); 
-
-    cout << "Enter album: ";
-    getline(cin,track.album);
-
-    cout << "Enter duration (seconds): ";
-    cin >> track.duration;
-    cin.ignore(numeric_limits<streamsize>:: max(), '\n');
-
-    cout << "Enter genre: ";
-    getline(cin,track.genre);
-
-    cout << "Enter release year: ";
-    cin >> track.releaseYear;
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-
-    library.push_back(track);
-    cout << "Track added successfully.\n\n";
-}
-
-void displayLibrary(const vector<MusicTrack>& library) {
-    //shows every track stored in the library
-    if (library.empty() ) {
-        cout << "The library is empty.\n\n";
-        return;
-    }
-
-    cout << "\n==== Music Library (" << library.size() << " tracks) ====\n";
-    for (size_t i = 0; i < library.size(); ++i) {
-        const MusicTrack& t = library[i];
-        cout << i + 1 << ". " << t.title << " - " << t.artist << "\n";
-        cout << "   Album: " << t.album << "\n";
-        cout << "   Duration: " << t.duration << " sec\n";
-        cout << "   Genre: " << t.genre << "\n";
-        cout << "   Release Year: " << t.releaseYear << "\n";
-    }
-    cout << "\n";
-}
-
-void writesStringToFile(ofstream& out, const string& str) {
-    size_t len = str.size();
-    out.write(reinterpret_cast<const char*>(&len), sizeof(len));
-    out.write(str.c_str(), static_cast<streamsize>(len));
-}
-
-string readStringFromFile(ifstream& in) {
-    size_t len = 0;
-    in.read(reinterpret_cast<char*>(&len), sizeof(len));
-    string str(len, '\0');
-    if (len > 0) {
-        in.read(&str[0], static_cast<streamsize>(len));
-    }
-    return str;
-}
-
-void saveLibraryToFile(const vector<MusicTrack>& library, const string& filename) {
-ofstream outFile(filename, ios::binary | ios::trunc);
-    if (!outFile) {
-        cout << "Error: could not open file for writing.\n\n";
-        return;
-}
-
-
-size_t count = library.size();
-outFile.write (reinterpret_cast<const char*>(&count), sizeof(count));
-
-for (const MusicTrack& t : library) {
-    writesStringToFile(outFile, t.title);
-    writesStringToFile(outFile, t.artist);
-    writesStringToFile(outFile, t.album);
-    outFile.write(reinterpret_cast<const char*> (&t.duration), sizeof(t.duration));
-     writesStringToFile(outFile, t.genre);
-     outFile.write(reinterpret_cast<const char*> (&t.releaseYear), sizeof(t.releaseYear));
-}
-
-outFile.close();
-cout << "Library saved to " << filename << " (" << count << " tracks).\n\n";
-}
-
-void loadLibraryFromFile(vector<MusicTrack>& library, const string& filename) {
-    ifstream inFile(filename, ios::binary);
-    if (!inFile) {
-        cout << "Error: could not open file for reading. Has it been saved yet?\n\n";
-        return;
-    }
-
-    library.clear();
-    
-    size_t count = 0;
-    inFile.read(reinterpret_cast<char*>(&count), sizeof(count));
-
-    for (size_t i = 0; i < count && inFile; ++i) {
-        MusicTrack t;
-        t.title = readStringFromFile(inFile);
-        t.artist = readStringFromFile(inFile);
-        t.album = readStringFromFile(inFile);
-        inFile.read(reinterpret_cast<char*>(&t.duration), sizeof(t.duration));
-        t.genre = readStringFromFile(inFile);
-        inFile.read(reinterpret_cast<char*>(&t.releaseYear), sizeof(t.releaseYear));
-        library.push_back(t);
-
-    }
-inFile.close();
-cout << ":Loaded " << library.size() << " tracks from " << filename << ".\n\n";
 }
